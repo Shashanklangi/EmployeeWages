6e637688a3da7beda1fa654afbaf0329344168f5
Shashanklangi <langishashank@gmail.com>
2022-04-30 13:34:57 +0530

    [Shashank]: Added implemented employee wage per month in Employee Wages

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -32,3 +32,30 @@ esac
 Daily_Wage=$(( $Emp_Hr * $Wage_Per_Hour )) 
 
 echo "Daily wage using switch case" $Daily_Wage
+
+#UC5
+#isPartTime=1
+#isFullTime=2
+totalsalary=0
+#empRatePerHr=20
+numofWorkingDays=20
+
+for (( day=1; day<=$numofWorkingDays; day++ ))
+do
+       #randomCheck=$((1+$RANDOM%2))
+       #case $randomCheck in
+                            #$isPartTime )
+                                   #empHrs=4
+                             #;;
+                            #$isFullTime )
+                                    #empHrs=8
+                              #;;
+                              #*)
+                                    #empHrs=0
+                               #;;
+     #esac
+salary=$(( $Emp_Hr * $Wage_Per_Hour ))
+totalsalary=$(( $totalsalary + $salary ))
+done
+#totalsalary=$(( $totalsalary + $salary ))
+echo "Total salary of month :" $totalsalary
